7bdbc61853de8b0286fa52792d92dcc346676196
VladimirGerman <vladimir.german.and@yandex.ru>
2024-11-26 21:40:02 +0700

    Начал заново

--- app/config.py
+++ app/config.py
@@ -1,10 +1,22 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    DB_HIST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
     @property
     def DATABASE_URL_asyncpg(self):
-        return 'sqlite+psycopg:///:memory:'
+        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HIST}:{self.DB_PORT}/{self.DB_NAME}'
+
+    @property
+    def DATABASE_URL_psycopg(self):
+        return f'postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HIST}:{self.DB_PORT}/{self.DB_NAME}'
+
+    model_config = SettingsConfigDict(env_file='.env')
 
 
 settings = Settings()
--- app/database.py
+++ app/database.py
@@ -1,15 +1,12 @@
-# from app.config import settings
+from config import settings
 from sqlalchemy import URL, create_engine, text
 from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
                                     create_async_engine)
 from sqlalchemy.orm import Session, sessionmaker
 
 engine = create_engine(
-    # url=settings.DATABASE_URL_asyncpg,
-    url='sqlite+psycopg:///:memory:',
+    url=settings.DATABASE_URL_psycopg,
     echo=True,
-    # pool_size=1,
-    # max_overflow=0,
 )
 
 with engine.connect() as conn:
